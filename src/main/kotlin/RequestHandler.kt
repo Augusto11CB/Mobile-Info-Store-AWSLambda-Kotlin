e1814ea1ce55b9bd73746051280a45b8a59f8ba8
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2020-04-03 23:06:34 -0300

    new Request handler

--- src/main/kotlin/RequestHandler.kt
+++ src/main/kotlin/RequestHandler.kt
@@ -1,13 +1,13 @@
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
-import model.dto.toJSON
+import model.dto.AppDataInfoRequestDTO
+import model.dto.AppDataInfoResponseDTO
 import repository.AppDataInfoRepository
 import repository.impl.AppDataInfoRepositoryImpl
 import service.VersionManager
 
 
+/*
 class RequestHandler() : RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     private var appDataInfoRepository: AppDataInfoRepository = AppDataInfoRepositoryImpl()
@@ -24,6 +24,26 @@ class RequestHandler() : RequestHandler<APIGatewayProxyRequestEvent, APIGatewayP
         return APIGatewayProxyResponseEvent()
             .withBody(appDataInfoDTO.toJSON())
             .withStatusCode(200)
+    }
+
+}
+*/
+
+
+class RequestHandler() : RequestHandler<AppDataInfoRequestDTO, AppDataInfoResponseDTO> {
+
+    private var appDataInfoRepository: AppDataInfoRepository = AppDataInfoRepositoryImpl()
+    private lateinit var versionManager: VersionManager
+
+    init {
+        versionManager = VersionManager(appDataInfoRepository)
+    }
+
+    override fun handleRequest(input: AppDataInfoRequestDTO?, context: Context?): AppDataInfoResponseDTO {
+
+        return versionManager.verifyAppVersion(input!!.userAgent)
+
+
     }
 
 }
